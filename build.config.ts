4d7ace191f3fa7b12230478a3409171d7f19bcf8
shuangxi <double8295@gmail.com>
2023-07-17 19:21:10 +0700

    flx_

--- build.config.ts
+++ build.config.ts
@@ -1,6 +1,6 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-    externals: ['path', 'node','node:*', 'node:fs', 'node:url', 'vite-plugin-vue-svg-icons', 'fs-extra', 'rollup-plugin-copy'],
+    externals: ['path', 'node','node:*', 'node:fs', 'node:url', 'vite-plugin-vue-svg-icons', 'fs-extra'],
     // failOnWarn: false
 });
